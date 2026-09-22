8d01e8009018f3db1c414199e36cc2526daf85e7
ArkadiDoktor <doktorovicharkadi@gmail.com>
2026-09-22 14:45:15 +0300

    Update interview_mirror_app.py

--- interview_mirror_app.py
+++ interview_mirror_app.py
@@ -66,7 +66,7 @@ if "GROQ_API_KEY" in st.secrets:
 else:
     api_key = os.environ.get("GROQ_API_KEY", "YOUR_LOCAL_FALLBACK_KEY")
 
-LLM_MODEL    = "llama-3.3-70b-versatile"
+LLM_MODEL    = "meta-llama/llama-4-scout-17b-16e-instruct"
 STT_MODEL    = "whisper-large-v3-turbo"
 
 # 2. Initialize the Groq Client safely
